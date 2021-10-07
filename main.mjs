42b9b35782402a3823f2ae8c3cec28c0e034d0f3
Adam Lacoste <adam.lacoste@chron.com>
2021-10-07 15:01:27 -0500

    Added main.mjs as package entry point

---
+++ main.mjs
@@ -0,0 +1,30 @@
+/**
+ *  Main entrypoint module
+ *
+ *  @author Adam Lacoste <adam.lacoste@hotmail.com>
+ *
+ *  @module card-sharp
+ *
+ *  @requires deep-extend
+ *  @requires card-sharp/lib/card
+ *  @requires card-sharp/lib/deck
+ */
+
+import extend    from "deep-extend";
+import Card      from "./lib/card.mjs";
+import Deck      from "./lib/deck.mjs";
+import std_deck  from "./config/decks/standard.json";
+import stdj_deck from "./config/decks/standard_jokers.json";
+
+const config     = { "decks": { "standard": std_deck, "standard_jokers": stdj_deck } },
+    default_opts = { "cards": stdj_deck };
+
+function init(user_opts = {}) {
+    let opts  = extend({}, default_opts, user_opts),
+        cards = Card.fromArray(opts.cards);
+
+    return new Deck(cards);
+} // end function init
+
+export { Card, Deck, config, init };
+export default init;
